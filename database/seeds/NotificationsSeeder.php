8ecb7d0b0b87e04aa9eb71a76225623d531611f8
Pablo Diaz <pablo@ultimatejetvacations.com>
2020-06-19 10:28:11 -0400

    Implemented the feeder for the notifications.

--- database/seeds/NotificationsSeeder.php
+++ database/seeds/NotificationsSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class NotificationsSeeder extends Seeder
@@ -11,6 +12,27 @@ class NotificationsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users = User::all()->map(function ($item, $key) {
+            return $item->id;
+        });
+
+        for($i = 1; $i <= sizeof($users); $i++)
+        {
+            $notificationsAmount = rand(3, 10);
+
+            for($y = 1; $y <= $notificationsAmount; $y++)
+            {
+                $statusValue = rand(0, 1);
+
+                $faker = Faker\Factory::create();
+
+                \App\UserNotification::create([
+                    "user_id"       => $i,
+                    "notifications" => $faker->sentence,
+                    "status"        => $statusValue,
+                ]);
+            }
+
+        }
     }
 }
